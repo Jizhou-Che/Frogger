e5efdfa179873bd3e3b28f95b3eea5e5445d277b
Jizhou-Che <che_mail@icloud.com>
2019-11-16 00:04:22 +0000

    G52SWM CW2 initiated.

---
+++ src/scyjc1/frogger/End.java
@@ -0,0 +1,28 @@
+package scyjc1.frogger;
+
+import javafx.scene.image.Image;
+
+public class End extends Actor{
+	boolean activated = false;
+	@Override
+	public void act(long now) {
+		// TODO Auto-generated method st
+	}
+	
+	public End(int x, int y) {
+		setX(x);
+		setY(y);
+		setImage(new Image("file:src/scyjc1/frogger/End.png", 60, 60, true, true));
+	}
+	
+	public void setEnd() {
+		setImage(new Image("file:src/scyjc1/frogger/FrogEnd.png", 70, 70, true, true));
+		activated = true;
+	}
+	
+	public boolean isActivated() {
+		return activated;
+	}
+	
+
+}
